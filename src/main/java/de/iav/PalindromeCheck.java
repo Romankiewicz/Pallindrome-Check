7c18ba4be0c75f9702b3658992d97ac1d6e686a7
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-05-06 08:36:21 +0200

    Add checkForLetter

--- src/main/java/de/iav/PalindromeCheck.java
+++ src/main/java/de/iav/PalindromeCheck.java
@@ -2,12 +2,23 @@ package de.iav;
 
 public class PalindromeCheck {
     public static boolean isPalindrome(String stringToCheck) {
-        if (stringToCheck.matches(" ")) {
+/*        if (stringToCheck.matches(" ")) {
             return true;
         }else if (stringToCheck.matches("")){
             return true;
         }else {
             return false;
         }
+    }*/
+        if (stringToCheck == " ") {
+            return true;
+        }
+        int stringLength = stringToCheck.length();
+        for (int i = 0; i < stringLength; i++) {
+            if ((Character.isLetter(stringToCheck.charAt(i)) == true)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/de/iav/PalindromeCheckTest.java
+++ src/test/java/de/iav/PalindromeCheckTest.java
@@ -16,16 +16,6 @@ class PalindromeCheckTest {
     }
 
     @Test
-    void checkPalindrome_whenStringIsNull_thenReturnTrue(){
-        //given
-        String stringToCheck = "";
-        //when
-        boolean isPalindrome = PalindromeCheck.isPalindrome(stringToCheck);
-        //then
-        assertTrue(isPalindrome);
-    }
-
-    @Test
     void checkPalindrome_whenStringIsAsingleLetter_thenReturnTrue() {
         //given
         String stringToCheck = "a";
